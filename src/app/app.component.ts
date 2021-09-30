b62e804b1dde6cb66faf62eb6339b1d2c80f5027
akshaypatel-dev <akshaypatel989@gmail.com>
2021-09-30 14:21:39 +0100

    templte approch has been done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { TemplatedrivenComponent } from './templatedriven/templatedriven.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const approute: Routes = [
+  {path: 'templatedriven',
+    component: TemplatedrivenComponent},
+  {path: 'reactive',
+    component: ReactiveComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatedrivenComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(approute),
   ],
   providers: [],
   bootstrap: [AppComponent]
